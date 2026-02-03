b26f5805e20b6445297590833b45ff4846c0b3ba
Pooja Maskare <your-email@example.com>
2026-02-03 20:43:41 +0530

    feat: Implement PDF document processing worker using Langchain and BullMQ, and configure project dependencies.

--- server/worker.js
+++ server/worker.js
@@ -1,13 +1,21 @@
 import { Worker } from "bullmq";
-
+import { QdrantVectorStore } from "@langchain/qdrant";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { Document } from "@langchain/core/documents";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf"
+import { CharacterTextSplitter } from "@langchain/textsplitters";
 const worker = new Worker('file-upload-queue',
     async job => {
+        console.log("Worker successfully started and processing job...");
         console.log(`job:`, job.data);
-        const data=JSON.parse(job.data);
-        console.log(data);
-
+        const data = JSON.parse(job.data);
+        const loader = new PDFLoader(data.path);
+        const docs = await loader.load();
+        const splitter = new CharacterTextSplitter({ chunkSize: 300, chunkOverlap: 0 });
+        const texts = await splitter.splitDocuments(docs);
+        console.log(`texts:`, texts);
     }, {
-        concurrency: 100,
+    concurrency: 100,
     connection: {
         host: "localhost",
         port: 6379,
